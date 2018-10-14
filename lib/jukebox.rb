99ca3e75615b4a055c81735fb8795b91c48ccfe9
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-14 21:30:17 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -26,7 +26,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   resp=gets.chomp
-  if songs.contains?(resp)
+  if songs.include?(resp)
     puts "Playing #{song}"
   else puts "Invalid input, please try again"
 end
